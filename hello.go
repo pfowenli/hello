134ea35ea6cd6365e2e6d558ba0eb2364436ed2d
Owen P. F. Li <pfowenli@gmail.com>
2022-03-28 23:46:00 +0800

    import package github.com/google/go-cmp/cmp from module github.com/google/go-cmp

--- hello.go
+++ hello.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"example/user/hello/morestrings"
+"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+        fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
